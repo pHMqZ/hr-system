afc91e6bcd952eac1388047e4926ce0225dbdddb
Phillip Marques <phillip.silva@capgemini.com>
2023-03-14 15:07:43 -0300

    Exception handling and Service Layer

--- payroll-api/src/main/java/com/pms/payrollapi/controller/PayrollController.java
+++ payroll-api/src/main/java/com/pms/payrollapi/controller/PayrollController.java
@@ -9,29 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pms.payrollapi.entity.Payroll;
-import com.pms.payrollapi.entity.User;
-import com.pms.payrollapi.feignClients.UserFeign;
+import com.pms.payrollapi.service.PayrollService;
 
 @RestController
 @RequestMapping("/api/payments")
 public class PayrollController {
 	
 	@Autowired
-	private UserFeign userFeign;
+	private PayrollService payrollService;
 	
 	@GetMapping("/{workerId}")
 	public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId,
 			@RequestBody Payroll payment){
-		
-		User user = userFeign.findById(workerId).getBody();
-		
-		return ResponseEntity.ok().body(
-				new Payroll(user.getName(), 
-						payment.getDescription(), 
-						user.getPriceHour(), 
-						payment.getWorkedHours(),
-						user.getPriceHour()* payment.getWorkedHours())
-				);
+		return ResponseEntity.ok().body(payrollService.getPayment(workerId, payment));
 	}
 
 }
---
+++ payroll-api/src/main/java/com/pms/payrollapi/exception/StandardError.java
@@ -0,0 +1,36 @@
+package com.pms.payrollapi.exception;
+
+import java.time.LocalDateTime;
+
+public class StandardError {
+	
+	private LocalDateTime timestamp;
+	private String error;
+	private Integer status;
+	private String path;
+	
+	public StandardError(LocalDateTime timestamp, String error, Integer status, String path) {
+		super();
+		this.timestamp = timestamp;
+		this.error = error;
+		this.status = status;
+		this.path = path;
+	}
+
+	public LocalDateTime getTimestamp() {
+		return timestamp;
+	}
+
+	public String getError() {
+		return error;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public String getPath() {
+		return path;
+	}
+	
+}
